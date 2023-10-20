aeb66285b5d702727fa428f47ec83c72bd8620b4
Ziya Balta <79661362+ziyaBalta@users.noreply.github.com>
2023-10-20 20:59:37 +0300

    Business Katmanının Oluşturulması

---
+++ Person.Business/DTO/PersonsDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person.Business.DTO
+{
+    public class PersonsDto
+    {
+        public int Id { get; set; }
+        public string TCNo { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
