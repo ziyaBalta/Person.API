b0e3c3a66a0378355311286fd79fe6a3528ddf14
Ziya Balta <79661362+ziyaBalta@users.noreply.github.com>
2023-10-20 21:26:05 +0300

    BusinessLayer

---
+++ Person.Business/Dto/ContactInformationDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person.Business.Dto
+{
+    public class ContactInformationDto
+    {
+        public int Id { get; set; }
+        public string Street { get; set; }
+        public string District { get; set; }
+        public string Province { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+    }
+}
---
+++ Person.Business/Dto/PersonsDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person.Business.Dto
+{
+    public class PersonsDto
+    {
+        public int Id { get; set; }
+        public string TCNo { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
